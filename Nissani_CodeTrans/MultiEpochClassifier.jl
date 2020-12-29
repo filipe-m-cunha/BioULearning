c8b1c5e251f1aec86d14c2b6202e48081ec0f1f7
filipe-m-cunha <filipe.miranda.cunha@tecnico.ulisboa.pt>
2020-12-29 07:58:04 +0000

    Light Debugging Done

--- Nissani_CodeTrans/MultiEpochClassifier.jl
+++ Nissani_CodeTrans/MultiEpochClassifier.jl
@@ -2,6 +2,7 @@ using DataFrames
 using CSV
 using HTTP
 using MLDataUtils
+using LinearAlgebra
 
 data_link = "http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 iris = DataFrame!(CSV.File(HTTP.get(data_link).body; header = false))
@@ -16,15 +17,16 @@ Xs, Ys = shuffleobs((X, Y))
     start_from_first_batch = 1
 
     if start_from_first_batch == 1
-        classifier_features_file = Xs
 
         epoch_nr = 1
         nmr_training_batches = 10
         test_batch_size = 10
+        total_train_batch_nmr = epoch_nr*nmr_training_batches
         classes = ["Iris-setosa", "Iris-virginica"]
         d = 4
         train_batch_size = 10
         nr_Classes = 2
+        display_class_names = 0
 
         if display_class_names == 1
             println(nr_Classes, "Classes, ")
@@ -47,14 +49,19 @@ Xs, Ys = shuffleobs((X, Y))
         μᵉpar = 4
 
         E_start = 150
-        Time_vary = 0
+        R_start = 200
+        time_var = 1
+
+        ϵvar = exp(log(0.1228)/total_train_batch_nmr)
+        αvar = exp(log(0.5604)/total_train_batch_nmr)
+        Φvar = 1
 
         nr_neurons = d*hyp_nmr
 
         w_N = zeros(d, nr_neurons)
 
         for i in 1:d:nr_neurons-1
-            w_N[:, i:(i+d-1)] = I(d)
+            w_N[:, i:(i+d-1)] = Diagonal(ones(d))
         end
 
         θₙ = θspacing*ones(d, hyp_nmr)
@@ -76,18 +83,18 @@ Xs, Ys = shuffleobs((X, Y))
 
     end
 
-    for j in 1:nmr_training_batches*epoch_nr
+    for j in 1:total_train_batch_nmr
         if time_var ==1
-            ϵ = ϵ*ϵvar^(j-1)
-            α = α*αvat^(j-1)
-            Φ = Φ*Φvar^(j-1)
+            ϵ *= ϵvar^(j-1)
+            α *= αvar^(j-1)
+            Φ *= Φvar^(j-1)
         end
 
         for k in 1:nr_neurons
-            ss = ss + 1
-
-            (w_N[:, k], θₜ[k], μ₁[:, k], μ₂[:, k], c1[k], c2[k], tₙ[k]) = NeuronLearningCycle(x,w_N[:, k], θₜ[k], μ₁[:, k], μ₂[:, k], c1[k], c2[k], tₙ[k], Φ, ϵ, α,  μᵉmode, μᵉpar, R_start, E_start)
-            (y_N[k], wx_N[k]) = NeuronActivity(x, w_N[:, k], θₙ[k])
+            ss += 1
+            println("ss= ", ss)
+            (w_N[:, k], θₜ[k], μ₁[:, k], μ₂[:, k], c1[k], c2[k], tₙ[k]) = NeuronLearningCycle(cv_X[:, k],w_N[:, k], θₜ[k], μ₁[:, k], μ₂[:, k], c1[k], c2[k], tₙ[k], Φ, ϵ, α,  μᵉmode, μᵉpar, R_start, E_start)
+            (y_N[k], wx_N[k]) = NeuronActivity(cv_X[:, k], w_N[:, k], θₙ[k])
         end
     end
 end
