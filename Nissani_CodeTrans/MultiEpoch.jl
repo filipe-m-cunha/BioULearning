68ca4d877deb73b7583b0a089ea6909f2186510f
filipe-m-cunha <filipe.miranda.cunha@tecnico.ulisboa.pt>
2021-02-16 10:01:37 +0000

    Added more flexibility to hiperplane initialization

--- Nissani_CodeTrans/MultiEpoch.jl
+++ Nissani_CodeTrans/MultiEpoch.jl
@@ -1,6 +1,60 @@
 include("Neuron_Learning_Cycle.jl")
 include("NeuronActivity.jl")
 
+
+function normalize(w::Array{Float64,2})
+    wnew = zeros(size(w)[1], size(w)[2])
+    for i in 1:size(w)[2]
+        wnew[:, i] = w[:, i]./sum(w[:, i])
+    end
+    return wnew
+end
+
+function hyperplane_inicialization(d::Int64, nmr_hyp::Int64, orientation = "random", Ω::Float64 = 2.0, θshift=0.1)
+
+    nr_neurons = d*nmr_hyp
+
+    if (orientation == "random")
+        w = normalize(rand(d, nr_neurons))
+        θ = Ω.*rand(nr_neurons)
+
+    elseif (orientation == "grid")
+        w = zeros(d, nr_neurons)
+        for i in 1:d:nr_neurons-1
+            w[:, i:(i+d-1)] = Diagonal(ones(d))
+        end
+        θspacing = Ω/(nmr_hyp + 1)
+        θ = θspacing*ones(d, nmr_hyp)
+        for i in 1:nmr_hyp
+            θ[:, i] = i*θ[:, i]
+        end
+        θ = reshape(θ, size(θ)[1]*size(θ)[2], 1)
+        θ = θ .+ θshift
+    else
+        @assert false "orientation has to be either random or grid!"
+    end
+    return w, θ
+end
+
+function hyperplane_additional_shift(training_set, w, θ, ϕ, Ω)
+
+    indices = rand(1:size(training_set)[1])
+    close = zeros(size(w)[2])
+    for i in 1:size(w)[2]
+        for j in indices
+            if (transpose(w[:, i])*training_set[j] ≥ (θ[i] - ϕ)) & (transpose(w[:, i])*training_set[j]  ≤ (θ[i] + ϕ))
+                close[i] = 1
+                break
+            end
+        end
+        if close[i] == 0
+            w[:, i] = rand(size(w)[2])
+            w[:, i] = w[:, i]./sum(w[:, i])
+            θ[i] = Ω*rand()
+        end
+    end
+    return w, θ
+
 function MultiEpoch(training_set, nmr_training_batches::Int64, d::Int64,
                     size_training_batch::Int64, nmr_epochs::Int64, nmr_hyp::Int64=8,
                     Ω::Float64=0.05,  σ::Float64=0.8, μᵉmode::Float64=0.0, μᵉpar::Float64=4.0, 
